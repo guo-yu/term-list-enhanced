63d86f2caa809d16d5ebe7be07c1a23242ad0b94
Guo Yu <i@guoyu.me>
2014-04-03 01:21:21 +0800

    0.0.2: support append, add examples

--- examples/menu.js
+++ examples/menu.js
@@ -1,10 +1,44 @@
 var List = require('../index');
-var menu = new List();
+var menu = new List({
+  labelKey: 'label'
+});
 
-menu.adds()
+menu.adds([
+  'Welcome to Term List Enhanced',
+  '==============================',
+  'this line is focused by default', 
+  {
+    label: '+ : click me to add new line',
+    add: true
+  }, {
+    label: '√ : click me to update lebel',
+    update: true
+  }
+]);
+
+menu.on('keypress', function(key, index) {
+  if (key.name === 'return') {
+    var item = menu.item(index);
+    if (item.add) {
+      return menu.append({
+        label: '+++ a new line, click to remove me',
+        remove: true
+      });
+    }
+    if (item.update) {
+      return menu.update(index, '| wooha !!')
+    }
+    if (item.remove) {
+      return menu.remove(index);
+    }
+    menu.update(index, 'you\'ve choose the ' + (index + 1) + 'th');
+  } else if (key.name === 'q') {
+    return menu.exit();
+  }
+})
 
 menu.on('empty', function() {
   menu.stop();
 });
 
-menu.start()
+menu.start(2)
--- libs/term-list.js
+++ libs/term-list.js
@@ -1,76 +1,75 @@
+var _ = require('underscore');
 var List = require('term-list');
 
 var defaults = {};
 defaults.marker = '\033[36m› \033[0m', // ♪
 defaults.markerLength = 2;
 defaults.labelMaxLength = 13;
+defaults.labelKey = 'name';
+defaults.max = 16;
 
 module.exports = TermList;
 
 function TermList(params) {
-  this.configs = params || defaults;
+  this.configs = _.extend(defaults, _.clone(params));
   this.menu = new List(this.configs);
   this.items = [];
 }
 
-TermList.prototype.adds = function(items, max, labelKey) {
-  
+TermList.prototype.add = function(item, index) {
+
+  if (!item) return false;
+  if (!this.menu) return false;
+
+  var currentLength = this.count();
+  var limit = this.configs.max;
+
+  if (currentLength > limit) return false;
+  if (index && (index + 1) > limit) return false;
+
+  var lastest = index || this.items.length;
+  if (_.isObject(item)) item.index = lastest;
+
+  var labelKey = this.configs.labelKey;
+  var label = _.isString(item) ? item : item[labelKey];
+  this.menu.add(lastest, label);
+  this.items.push(item);
+
+  return this;
+
+}
+
+TermList.prototype.adds = function(items) {
   if (!items) return false;
   if (items.length === 0) return false;
-  
   var self = this;
-  var menu = this.menu;
-  var limit = max || 16;
-  var label = labelKey || 'name';
-  
   items.forEach(function(item, index) {
-    if (index > limit) return false;
-    if (typeof(item) === 'object') item.index = index;
-    self.items.push(item);
-    menu.add(index, typeof(item) === 'string' ? item : item[label]);
+    self.add(item, index);
   });
-
   return this;
-}
-
-TermList.prototype.on = function(event, callback) {
-  this.menu.on(event, callback);
-  return this;
-}
-
-TermList.prototype.start = function(by) {
-  this.menu.start();
-  if (by) this.menu.select(by);
-  return this;
-}
-
-TermList.prototype.stop = function() {
-  if (this.menu) this.menu.stop();
-  return false;
 }
 
 TermList.prototype.update = function(index, text, align) {
-  
+
   var menu = this.menu;
   var item = this.items[index];
-  
-  if (!item) return false;
-  
-  var original = (typeof(item) === 'string') ? item : item.name;
-  var str = text ? ' ' + text : '';
-  
-  menu.at(index).label = 
-    align ?
-    this.align(original, this.configs.labelMaxLength || 13) + str :
-    original + str;
-  menu.draw();
+  var label = this.configs.labelKey;
+  var line = menu.at(index);
 
+  if (!item) return false;
+  if (!line) return false;
+
+  var original = (typeof(item) === 'string') ? item : item[label];
+  var str = text ? ' ' + text : '';
+
+  line.label =
+    align ?
+    this.align(original, this.configs.labelMaxLength) + str :
+    original + str;
+
+  menu.draw();
   return false;
 
-}
-
-TermList.prototype.clear = function(index) {
-  return this.update(index);
 }
 
 TermList.prototype.align = function(str, maxlength) {
@@ -89,4 +88,54 @@ TermList.prototype.align = function(str, maxlength) {
     return aligned;
   }
 
+}
+
+TermList.prototype.count = function () {
+  if (!this.items || this.items.length === 0) return 0;
+  var count = this.items.filter(function(item) {
+    return item != null;
+  });
+  return count.length;
+}
+
+TermList.prototype.item = function(key, value) {
+  if (key && value) this.items[key] = value;
+  return this.items[key];
+}
+
+TermList.prototype.on = function(event, callback) {
+  this.menu.on(event, callback);
+  return this;
+}
+
+TermList.prototype.start = function(by) {
+  this.menu.start();
+  if (by) this.menu.select(by);
+  return this;
+}
+
+TermList.prototype.append = function(item) {
+  if (!this.menu) return false;
+  this.add(item);
+  this.menu.draw();
+}
+
+TermList.prototype.stop = function() {
+  if (this.menu) this.menu.stop();
+  return false;
+}
+
+TermList.prototype.clear = function(index) {
+  return this.update(index);
+}
+
+TermList.prototype.remove = function(index) {
+  if (!this.menu) return false;
+  delete this.items[index];
+  return this.menu.remove(index);
+}
+
+TermList.prototype.exit = function() {
+  this.menu.stop();
+  return process.exit();
 }
